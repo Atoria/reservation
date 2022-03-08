58dae525bda9193974559983760087301c4af011
AToria <andria.toria@singular.uk>
2022-03-08 04:40:18 +0400

    Add key

---
+++ migrations/20220308003813-add_unique_ticket_id_column.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.sequelize.query(`create unique index reserved_ticket_ticket_id_uindex
+                                           on reserved_ticket (ticket_id);`);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.sequelize.query(`ALTER TABLE reserved_ticket DROP INDEX reserved_ticket_ticket_id_uindex;`);
+  }
+};
